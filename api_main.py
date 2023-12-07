8817b0cae7b2b2612beaf3fe58e51bddd8b58de3
A-Salty-Fish <1309032417@qq.com>
2023-12-07 18:32:49 +0800

    feat:get output

--- api_main.py
+++ api_main.py
@@ -34,11 +34,24 @@ def load_oss_config():
         config = yaml.load(config_file.read(), Loader=yaml.Loader)
         return config['oss']
 
+def get_output(config_name):
+    output_dirs = os.listdir('./samples')
+    outputs = []
+    for output_dir in output_dirs:
+        if output_dir.startswith(config_name):
+            directory = './samples/' + output_dir
+            for root, _, files in os.walk(directory):
+                for file in files:
+                    if file.endswith(".mp4"):
+                        outputs.append(os.path.join(root, file))
+    return outputs
+
 
 if __name__ == '__main__':
-    oss_config = load_oss_config()
-    auth = oss_util.init_auth(oss_config['access_key_id'], oss_config['access_key_secret'])
-    print(oss_util.upload_file(auth, oss_config['end_point'] , oss_config['bucket_name'], "./inputs/applications/api_image/monalisa.png", "magic_api_result/monalisa.png"))
+    print(get_output('1'))
+    # oss_config = load_oss_config()
+    # auth = oss_util.init_auth(oss_config['access_key_id'], oss_config['access_key_secret'])
+    # print(oss_util.upload_file(auth, oss_config['end_point'] , oss_config['bucket_name'], "./inputs/applications/api_image/monalisa.png", "magic_api_result/monalisa.png"))
     # config_file = prompt_config_util.generate_config("monalisa.png",
     #                                                  "running.mp4",
     #                                                  "monalisatest",
